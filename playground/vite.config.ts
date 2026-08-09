941bc5f2c0de51d562c52c048e39c62f0c885c36
jeykori <5616230+jeykori@users.noreply.github.com>
2026-08-09 19:41:36 +0800

    init playground: vite + tailwindcss + shadcn

---
+++ playground/vite.config.ts
@@ -0,0 +1,20 @@
+import path from "node:path";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@jeykori/guildwars2-toolkit": path.resolve(__dirname, "../src"),
+		},
+	},
+});
